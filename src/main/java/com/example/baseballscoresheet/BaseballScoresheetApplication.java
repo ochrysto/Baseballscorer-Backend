837f2bbec2293a6c3715d5b57432d9aa67f32d50
darleen <darleen.e1@schule.bremen.de>
2024-03-06 09:34:02 +0100

    adds files

---
+++ src/main/java/com/example/baseballscoresheet/BaseballScoresheetApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseballscoresheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballScoresheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballScoresheetApplication.class, args);
+    }
+
+}
