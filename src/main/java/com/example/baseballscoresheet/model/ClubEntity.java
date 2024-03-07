ce80b73125dc9b8bf8e7c1cbae7c367e1226c058
darleen <darleen.e1@schule.bremen.de>
2024-03-07 11:04:10 +0100

    adds ClubEntity.java

---
+++ src/main/java/com/example/baseballscoresheet/model/ClubEntity.java
@@ -0,0 +1,35 @@
+package com.example.baseballscoresheet.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+//TODO bisschen mehr Infos, was ein Club ist / Unterscheidung von einem Team?
+/**
+ * Die Klasse {@link ClubEntity} bildet ein Club-Objekt mit seinen dazugehörigen Attributen ab.
+ *
+ */
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "club")
+public class ClubEntity {
+    /**
+     * Attribut, das zur eindeutigen Identifikation eines Club-Objekts dient.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Name des Clubs.
+     */
+    @NotBlank(message = "Name of the club is mandatory")
+    private String name;
+
+    //TODO 1:n- oder n:m-Beziehung?
+    private AssociationEntity association;
+}
