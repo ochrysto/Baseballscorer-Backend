bd8523178f260d53d02736647a218a87687d1007
darleen <darleen.e1@schule.bremen.de>
2024-03-07 12:37:44 +0100

    adds LeagueTeamEntity.java

---
+++ src/main/java/com/example/baseballscoresheet/model/LeagueTeamEntity.java
@@ -0,0 +1,38 @@
+package com.example.baseballscoresheet.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//TODO bisschen mehr Infos, was eine League ist?
+/**
+ * Die Klasse {@link LeagueTeamEntity} bildet die n:m-Beziehung zwischen League und Team mit ihren dazugehörigen Attributen ab.
+ * Es wird abgebildet, welche Teams zu welcher League gehören.
+ */
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "league_team")
+public class LeagueTeamEntity {
+    /**
+     * Attribut, das zur eindeutigen Identifikation einer League-Team-Beziehung dient.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //TODO was ist hier die Association?
+    private AssociationEntity association;
+
+    /**
+     * {@link TeamEntity} bildet das Team der Beziehung ab.
+     */
+    private TeamEntity team;
+
+    /**
+     * {@link LeagueEntity} bildet die League der Beziehung ab.
+     */
+    private LeagueEntity league;
+}
