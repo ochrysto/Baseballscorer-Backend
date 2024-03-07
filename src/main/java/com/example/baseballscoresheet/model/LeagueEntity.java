643bd1d1e67692181014d797fb2a4c2dac7dcb87
darleen <darleen.e1@schule.bremen.de>
2024-03-07 12:37:32 +0100

    adds LeagueEntity.java

---
+++ src/main/java/com/example/baseballscoresheet/model/LeagueEntity.java
@@ -0,0 +1,32 @@
+package com.example.baseballscoresheet.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//TODO bisschen mehr Infos, was eine League ist?
+
+/**
+ * Die Klasse {@link LeagueEntity} bildet ein League-Objekt mit seinen dazugehörigen Attributen ab.
+ */
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "league")
+public class LeagueEntity {
+    /**
+     * Attribut, das zur eindeutigen Identifikation eines League-Objekts dient.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    //TODO welches Team muss da hin?
+    private TeamEntity team;
+
+    //TODO was für eine E-Mail?
+    private String email;
+}
