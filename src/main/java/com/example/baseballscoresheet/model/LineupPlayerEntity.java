57d2516bdc0f0f7d0c83b0648188f1392fe03de0
darleen <darleen.e1@schule.bremen.de>
2024-03-07 12:38:59 +0100

    adds LineupPlayerEntity.java

---
+++ src/main/java/com/example/baseballscoresheet/model/LineupPlayerEntity.java
@@ -0,0 +1,40 @@
+package com.example.baseballscoresheet.model;
+
+import com.example.baseballscoresheet.enums.Position;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Die Klasse {@link LineupPlayerEntity} bildet die n:m-Beziehung zwischen Lineup und Player mit ihren dazugehörigen Attributen ab.
+ * Sie gibt an welche Spieler in welchem Lineup hinterlegt ist. Die Position gibt an, an welcher Position der Spieler spielt.
+ */
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "lineup_player")
+public class LineupPlayerEntity {
+    /**
+     * Attribut, das zur eindeutigen Identifikation einer Lineup-Player-Beziehung dient.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * {@link TeamEntity} bildet die zugehörige Team-Entity-Beziehung dieser Beziehung ab.
+     */
+    private TeamPlayerEntity teamPlayer;
+
+    /**
+     * {@link LineupEntity} bildet das Lineup dieser Beziehung ab.
+     */
+    private LineupEntity lineup;
+
+    /**
+     * {@link Position} spezifiziert die Position eines Spielers in dieser Beziehung.
+     */
+    private Position position;
+}
