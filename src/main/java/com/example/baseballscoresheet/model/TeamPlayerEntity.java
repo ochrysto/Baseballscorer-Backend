5ea5aef326534912cbc46cf633eb385b063af424
darleen <darleen.e1@schule.bremen.de>
2024-03-07 12:39:38 +0100

    adds TeamPlayerEntity.java

---
+++ src/main/java/com/example/baseballscoresheet/model/TeamPlayerEntity.java
@@ -0,0 +1,34 @@
+package com.example.baseballscoresheet.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Die Klasse {@link TeamPlayerEntity} bildet die n:m-Beziehung zwischen Team und Player mit ihren dazugehörigen Attributen ab.
+ * Sie gibt an, welche Spieler für welches Team spielen.
+ */
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "team_player")
+public class TeamPlayerEntity {
+    /**
+     * Attribut, das zur eindeutigen Identifikation einer Team-Player-Beziehung dient.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * {@link TeamEntity} bildet das Team dieser Beziehung ab.
+     */
+    private TeamEntity team;
+
+    /**
+     * {@link PlayerEntity} bildet den Spieler dieser Beziehung ab.
+     */
+    private PlayerEntity player;
+}
