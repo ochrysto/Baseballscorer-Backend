6ac33d81cdc0772bf7c854dd147f4f5f0116a67f
darleen <darleen.e1@schule.bremen.de>
2024-03-07 12:38:46 +0100

    adds LineupEntity.java

---
+++ src/main/java/com/example/baseballscoresheet/model/LineupEntity.java
@@ -0,0 +1,35 @@
+package com.example.baseballscoresheet.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//TODO bisschen mehr Infos zur Lineup Entität
+
+/**
+ * Die Klasse {@link LineupEntity} bildet ein Lineup-Objekt mit seinen dazugehörigen Attributen ab.
+ */
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "lineup")
+public class LineupEntity {
+    /**
+     * Attribut, das zur eindeutigen Identifikation eines Lineup-Objekts dient.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * {@link TeamEntity} bildet die Game-Team-Beziehung der Beziehung ab.
+     */
+    private GameTeamEntity gameTeam;
+
+    /**
+     * {@link ManagerEntity} bildet den Manager in der Beziehung ab.
+     */
+    private ManagerEntity manager;
+}
