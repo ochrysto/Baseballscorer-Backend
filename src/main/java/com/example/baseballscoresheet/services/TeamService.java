6d73400841ec515321067e2cd2706176b6a18fda
darleen <darleen.e1@schule.bremen.de>
2024-04-23 14:57:48 +0200

    adds findAll() and findAllManagersInTeams() methods to TeamService

--- src/main/java/com/example/baseballscoresheet/services/TeamService.java
+++ src/main/java/com/example/baseballscoresheet/services/TeamService.java
@@ -1,14 +1,18 @@
 package com.example.baseballscoresheet.services;
 
+import com.example.baseballscoresheet.model.ManagerEntity;
 import com.example.baseballscoresheet.model.TeamEntity;
 import com.example.baseballscoresheet.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TeamService {
 
-    private TeamRepository teamRepository;
+    private final TeamRepository teamRepository;
 
     @Autowired
     public TeamService(TeamRepository teamRepository) {
@@ -17,5 +21,19 @@ public class TeamService {
 
     public TeamEntity createTeam(TeamEntity teamEntity) {
         return this.teamRepository.save(teamEntity);
+    }
+
+    public List<TeamEntity> findAll() {
+        return this.teamRepository.findAll();
+    }
+
+    public List<ManagerEntity> findAllManagersInTeams() {
+        List<TeamEntity> allTeams = this.findAll();
+        List<ManagerEntity> allManagersInTeams = new ArrayList<>();
+
+        for (TeamEntity teamEntity : allTeams) {
+            allManagersInTeams.add(teamEntity.getManager());
+        }
+        return allManagersInTeams;
     }
 }
