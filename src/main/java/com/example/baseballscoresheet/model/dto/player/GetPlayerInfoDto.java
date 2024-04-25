b9ce965fecd77115e8ba9def41400cc17b7379ac
darleen <darleen.e1@schule.bremen.de>
2024-04-25 12:37:07 +0200

    adds GetPlayerInfoDto

---
+++ src/main/java/com/example/baseballscoresheet/model/dto/player/GetPlayerInfoDto.java
@@ -0,0 +1,11 @@
+package com.example.baseballscoresheet.model.dto.player;
+
+public class GetPlayerInfoDto {
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Integer passnumber;
+}
