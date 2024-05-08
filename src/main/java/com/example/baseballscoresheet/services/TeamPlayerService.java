a9149541f28f767dfd1369bdb972fadbcc332fe1
darleen <darleen.e1@schule.bremen.de>
2024-05-08 11:12:27 +0200

    adds repository for TeamPlayer

---
+++ src/main/java/com/example/baseballscoresheet/services/TeamPlayerService.java
@@ -0,0 +1,21 @@
+package com.example.baseballscoresheet.services;
+
+import com.example.baseballscoresheet.model.TeamPlayerEntity;
+import com.example.baseballscoresheet.repositories.TeamPlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeamPlayerService {
+    private final TeamPlayerRepository teamPlayerRepository;
+
+    @Autowired
+
+    public TeamPlayerService(TeamPlayerRepository teamPlayerRepository) {
+        this.teamPlayerRepository = teamPlayerRepository;
+    }
+
+    public void createTeamPlayer(TeamPlayerEntity teamPlayerEntity) {
+        this.teamPlayerRepository.save(teamPlayerEntity);
+    }
+}
