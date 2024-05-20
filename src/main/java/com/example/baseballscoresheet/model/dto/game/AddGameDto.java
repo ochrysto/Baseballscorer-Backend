ee9edb43d876ab8a0a4f79b8ce9073dee3027205
darleen <darleen.e1@schule.bremen.de>
2024-05-20 12:13:35 +0200

    fills AddGameDto with attributes deletes UpdateDto

--- src/main/java/com/example/baseballscoresheet/model/dto/game/AddGameDto.java
+++ src/main/java/com/example/baseballscoresheet/model/dto/game/AddGameDto.java
@@ -1,4 +1,27 @@
 package com.example.baseballscoresheet.model.dto.game;
 
+import java.util.Date;
+import java.util.List;
+
 public class AddGameDto {
+
+    private Long gameNumber;
+
+    private Date date;
+
+    private String location;
+
+    private Integer innings;
+
+    private Long associationId;
+
+    private Long leagueId;
+
+    private Long hostTeamId;
+
+    private Long guestTeamId;
+
+    private List<Long> umpireIdsList;
+
+    private Long scorerId;
 }
