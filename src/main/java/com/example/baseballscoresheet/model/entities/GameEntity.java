6d3974741afa461e741b057efde8cee6f8f21081
darleen <darleen.e1@schule.bremen.de>
2024-05-21 16:32:59 +0200

    remove unused import statements

--- src/main/java/com/example/baseballscoresheet/model/entities/GameEntity.java
+++ src/main/java/com/example/baseballscoresheet/model/entities/GameEntity.java
@@ -1,7 +1,6 @@
 package com.example.baseballscoresheet.model.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDate;
--- src/main/java/com/example/baseballscoresheet/services/AssociationService.java
+++ src/main/java/com/example/baseballscoresheet/services/AssociationService.java
@@ -2,7 +2,6 @@ package com.example.baseballscoresheet.services;
 
 import com.example.baseballscoresheet.model.entities.AssociationEntity;
 import com.example.baseballscoresheet.repositories.AssociationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
