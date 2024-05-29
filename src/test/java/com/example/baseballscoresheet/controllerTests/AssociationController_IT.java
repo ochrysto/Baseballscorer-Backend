f7235da877e9baa4177e3c55567979ed2687a428
YelSef <sefayel2002@gmail.com>
2024-05-29 12:42:51 +0200

    PositionController_IT Test added

--- src/test/java/com/example/baseballscoresheet/controllerTests/AssociationController_IT.java
+++ src/test/java/com/example/baseballscoresheet/controllerTests/AssociationController_IT.java
@@ -7,7 +7,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
