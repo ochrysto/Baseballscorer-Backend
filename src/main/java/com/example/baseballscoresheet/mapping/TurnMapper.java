542243cd62538b519d5dcf04e20d43044de2c067
waled98 <sadsoul304@gmail.com>
2024-06-05 10:34:48 +0200

    moin

--- src/main/java/com/example/baseballscoresheet/mapping/TurnMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.baseballscoresheet.mapping;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
-@Mapper
-public interface TurnMapper {
-    TurnMapper INSTANCE = Mappers.getMapper(TurnMapper.class);
-}
